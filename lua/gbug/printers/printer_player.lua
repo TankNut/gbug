ab2770493c8282d3fe188e5feb9e322578dfbdd8
TankNut <m.vlasblom@live.nl>
2022-09-30 18:53:41 +0200

    Print player nick when inline

--- lua/gbug/printers/printer_player.lua
+++ lua/gbug/printers/printer_player.lua
@@ -17,6 +17,6 @@ end
 function Inline(val)
 	return {
 		gbug.Colors.Value, "Player", color_white, "(", gbug.Colors.Print, tostring(val:UserID()), color_white, ") ",
-		gbug.Colors.Comment, string.format("--[[ %s, %s ]]", val:SteamID(), val:UserID())
+		gbug.Colors.Comment, string.format("--[[ %s, %s ]]", val:SteamID(), val:Nick())
 	}
 end
